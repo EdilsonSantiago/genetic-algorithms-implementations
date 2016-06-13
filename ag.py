bf967878ab43c41cae5929e2192dd1ab347f3a18
Edilson Santiago <edilson.engcomp@gmail.com>
2016-06-13 16:02:57 -0300

    precisao

--- ag.py
+++ ag.py
@@ -1,9 +1,16 @@
 __author__ = 'edilson'
 
-import numpy as np
+# import numpy as np
 from random import randint
 
 # z = 0.5 - (((np.sin(np.sqrt(x**2 + y**2)))**2 - 0.5) / (1 + (0.001 * (x**2 + y**2)) ** 2
+
+
+def bits_valores():
+    precisao_bits = [200 / ((2 ** 25) - 1)]
+    for k in range(1, 25):
+        precisao_bits.append(precisao_bits[k - 1] * 2)
+    return precisao_bits
 
 
 def criar_populacao(tamanho_populacao, tamanho_codigo):
@@ -18,7 +25,7 @@ def criar_populacao(tamanho_populacao, tamanho_codigo):
     return populacao_criada
 
 
-def avaliacao_aptidao(populacao):
+def avaliacao_aptidao():
     pass
 
 
@@ -36,3 +43,4 @@ def mutacao():
 
 populacao = criar_populacao(10, 50)
 print(populacao)
+print(bits_valores())
