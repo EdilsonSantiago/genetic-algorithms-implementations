d0ffcd85a3c56775d3708e2bafbe68f31ce96f1a
Edilson Santiago <edilson.engcomp@gmail.com>
2016-06-29 11:19:28 -0300

    melhor individuo

--- ag.py
+++ ag.py
@@ -2,30 +2,21 @@ __author__ = 'edilson'
 
 import numpy as np
 from random import randint, uniform
+import matplotlib
+import matplotlib.pyplot as plt
 
 
-tamanho_populacao = 100
+tamanho_populacao = 50
 tamanho_codigo = 50
-geracoes = 50
-i = 0
-
-def bits_valores():
-    precisao_bits = [200 / ((2 ** 25) - 1)]
-    for k in range(1, 25):
-        precisao_bits.append(precisao_bits[k - 1] * 2)
-    return precisao_bits
-
-
-def valor_da_variavel(vetor_precisao, cromossomo):
-    eixo_x = 0
-    eixo_y = 0
-    for i in range(0, 25):
-        if cromossomo[i]:
-            eixo_x += vetor_precisao[i]
-    for i in range(26, 50):
-        if cromossomo[i]:
-            eixo_y += vetor_precisao[i - 26]
-    return eixo_x, eixo_y
+geracoes = 1000
+geracao_atual = 0
+taxa_cruzamento = 75
+taxa_mutacao = 1
+apto = []
+media_apt = []
+posicao_x = []
+posicao_y = []
+eixo = np.linspace(0, 999, 1000)
 
 
 def criar_populacao(tamanho_populacao, tamanho_codigo):
@@ -40,6 +31,25 @@ def criar_populacao(tamanho_populacao, tamanho_codigo):
     return populacao_criada
 
 
+def bits_valores():
+    precisao_bits = [200 / ((2 ** 25) - 1)]
+    for k in range(1, 25):
+        precisao_bits.append(precisao_bits[k - 1] * 2)
+    return precisao_bits
+
+
+def valor_da_variavel(vetor_precisao, cromossomo):
+    eixo_x = 0
+    eixo_y = 0
+    for i in range(0, 25):
+        if cromossomo[i]:
+            eixo_x += vetor_precisao[i]
+    for i in range(26, 49):
+        if cromossomo[i]:
+            eixo_y += vetor_precisao[i - 26]
+    return eixo_x, eixo_y
+
+
 def avaliacao_aptidao(vetor_populacao):
     vetor_aptidao = []
     for i in range(0, len(vetor_populacao)):
@@ -50,55 +60,57 @@ def avaliacao_aptidao(vetor_populacao):
     return vetor_aptidao
 
 
-def selecao(populacao_selecao, vetor_aptidao, tamanho_populacao):
-    nova_populacao = []
-    soma = 0
-    var_1 = 0
-    acumulador = 0
+def selecao(populacao, vetor_aptidao):
+    soma, acumulador, ind = 0, vetor_aptidao[0], 0
+    selecionado = []
     for i in range(0, len(vetor_aptidao)):
         soma += vetor_aptidao[i]
-    while len(nova_populacao) < tamanho_populacao:
-        valor = uniform(0, soma)
-        acumulador += vetor_aptidao[var_1]
-        var_1 += 1
+    num_aleatorio = uniform(0, soma)
+    while len(selecionado) != 50:
+        acumulador += vetor_aptidao[ind]
+        if acumulador >= num_aleatorio:
+            selecionado = populacao[ind]
+            break
+        ind += 1
+    #print(selecionado)
+    return selecionado
 
-        if acumulador >= valor:
-            nova_populacao.append(populacao_selecao[var_1])
-            print(nova_populacao)
 
-    return nova_populacao
-
-
-def cruzamento(populacao_1, tamanho_populacao):
+def cruzamento(populacao_1, tamanho_populacao, taxa_cruzamento):
     contador = 0
-    nova_populacao = []
     filho_1 = []
     filho_2 = []
+    populacao_2 = []
+    aptidao = avaliacao_aptidao(populacao_1)
 
     while contador < tamanho_populacao:
         ponto = randint(1, 49)
-        pai_1 = populacao_1[contador]
-        pai_2 = populacao_1[contador + 1]
-        for i in range(0, ponto):
-            filho_1.append(pai_1[i])
-            filho_2.append(pai_2[i])
-        for i in range(ponto + 1, 50):
-            filho_1.append(pai_1[i])
-            filho_2.append(pai_2[i])
-        populacao_1.append(filho_1)
-        populacao_1.append(filho_2)
-        filho_1 = []
-        filho_2 = []
-        contador += 2
-    return populacao_1
+        vetor = selecao(populacao_1, aptidao)
+        pai_1 = vetor
+        vetor = selecao(populacao_1, aptidao)
+        pai_2 = vetor
+        aleatorio = randint(0, 100)
+        if aleatorio < taxa_cruzamento:
+            for i in range(0, ponto):
+                filho_1.append(pai_1[i])
+                filho_2.append(pai_2[i])
+            for i in range(ponto, 50):
+                filho_1.append(pai_1[i])
+                filho_2.append(pai_2[i])
+            populacao_2.append(filho_1)
+            populacao_2.append(filho_2)
+            filho_1 = []
+            filho_2 = []
+            contador += 2
+
+    return populacao_2
 
 
-def mutacao(vetor_populacao):
-    taxa_de_mutacao = 10
+def mutacao(vetor_populacao, taxa_de_mutacao):
     for i in range(0, len(vetor_populacao)):
-        numero_aleatorio = randint(0, 100)
+        numero_aleatorio = uniform(0, 100)
         if numero_aleatorio < taxa_de_mutacao:
-            indice = randint(0, 50)
+            indice = randint(0, 49)
             vetor = vetor_populacao[i]
             if vetor[indice] == 0:
                 vetor[indice] = 1
@@ -107,11 +119,40 @@ def mutacao(vetor_populacao):
             vetor_populacao[i] = vetor
     return vetor_populacao
 
+
+def mais_apto(aptidao):
+    apto = 0
+    for i in range(0, len(aptidao)):
+        if apto < aptidao[i]:
+            apto = aptidao[i]
+    return apto
+
+
 populacao = criar_populacao(tamanho_populacao, tamanho_codigo)
-while i == geracoes:
-    populacao = cruzamento(populacao, tamanho_populacao)
-    populacao = mutacao(populacao)
+
+for j in range(0, tamanho_populacao):
+        x, y = valor_da_variavel(bits_valores(), populacao[j])
+        posicao_x.append(x)
+        posicao_y.append(y)
+
+while geracao_atual < geracoes:
+    print(geracao_atual)
+    populacao = cruzamento(populacao, tamanho_populacao, taxa_cruzamento)
+    populacao = mutacao(populacao, taxa_mutacao)
     aptidao = avaliacao_aptidao(populacao)
-    populacao = selecao(populacao, aptidao, tamanho_populacao)
-    i += 1
-print(populacao)
+
+    apto.append(mais_apto(aptidao))
+    media_apt.append(sum(aptidao)/len(aptidao))
+    geracao_atual += 1
+
+"""
+matplotlib.rcParams['axes.unicode_minus'] = False
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(posicao_x, posicao_y, 'o')
+"""
+f, vetor = plt.subplots(2)
+vetor[0].plot(eixo, apto)
+vetor[1].plot(eixo, media_apt)
+
+plt.show()
