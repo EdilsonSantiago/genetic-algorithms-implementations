8478a5cd46ee15bd05e396f085315f39efa35afc
Edilson Santiago <edilson.engcomp@gmail.com>
2016-07-08 16:08:40 -0300

    ajustes

--- ag.py
+++ ag.py
@@ -8,13 +8,15 @@ import csv
 tamanho_populacao = 50
 tamanho_codigo = 50
 geracoes = 1000
+melhor_individuo = 0
+# melhor_individuo_cromossomo = []
 geracao_atual = 0
 taxa_cruzamento = 75
 taxa_mutacao = 1
 eixo = np.linspace(0, 999, 1000)
 
 
-def criar_populacao(tamanho_populacao, tamanho_codigo):
+def criar_populacao():
     populacao_criada = []
     lista = []
 
@@ -48,14 +50,13 @@ def valor_da_variavel(vetor_precisao, cromossomo):
 def avaliacao_aptidao(vetor_populacao):
     vetor_aptidao = []
     for i in range(0, len(vetor_populacao)):
-        aptidao = 0
         x, y = valor_da_variavel(bits_valores(), vetor_populacao[i])
-        aptidao = 0.5 - (((np.sin(np.sqrt(x**2 + y**2)))**2 - 0.5) / (1 + (0.001 * (x**2 + y**2)) ** 2))
-        vetor_aptidao.append(aptidao)
+        aptd = 0.5 - (((np.sin(np.sqrt(x**2 + y**2)))**2 - 0.5) / (1 + (0.001 * (x**2 + y**2)) ** 2))
+        vetor_aptidao.append(aptd)
     return vetor_aptidao
 
 
-def selecao(populacao, vetor_aptidao):
+def selecao(populacao_selecao, vetor_aptidao):
     soma, acumulador, ind = 0, vetor_aptidao[0], 0
     selecionado = []
     soma = sum(vetor_aptidao)
@@ -63,25 +64,25 @@ def selecao(populacao, vetor_aptidao):
     while len(selecionado) != 50:
         acumulador += vetor_aptidao[ind]
         if acumulador >= num_aleatorio:
-            selecionado = populacao[ind]
+            selecionado = populacao_selecao[ind]
             break
         ind += 1
-    #print(selecionado)
+    # print(selecionado)
     return selecionado
 
 
-def cruzamento(populacao_1, tamanho_populacao, taxa_cruzamento):
+def cruzamento(populacao_1):
     contador = 0
     filho_1 = []
     filho_2 = []
     populacao_2 = []
-    aptidao = avaliacao_aptidao(populacao_1)
+    aptidao_cruzamento = avaliacao_aptidao(populacao_1)
 
     while contador < tamanho_populacao:
         ponto = randint(1, 49)
-        vetor = selecao(populacao_1, aptidao)
+        vetor = selecao(populacao_1, aptidao_cruzamento)
         pai_1 = vetor
-        vetor = selecao(populacao_1, aptidao)
+        vetor = selecao(populacao_1, aptidao_cruzamento)
         pai_2 = vetor
         aleatorio = randint(0, 100)
         if aleatorio < taxa_cruzamento:
@@ -104,51 +105,53 @@ def mutacao(vetor_populacao, taxa_de_mutacao):
     for i in range(0, len(vetor_populacao)):
         numero_aleatorio = uniform(0, 100)
         if numero_aleatorio < taxa_de_mutacao:
-            indice = randint(0, 49)
+            indice_vetor = randint(0, 49)
             vetor = vetor_populacao[i]
-            if vetor[indice] == 0:
-                vetor[indice] = 1
+            if vetor[indice_vetor] == 0:
+                vetor[indice_vetor] = 1
             else:
-                vetor[indice] = 0
+                vetor[indice_vetor] = 0
             vetor_populacao[i] = vetor
     return vetor_populacao
 
 
-def mais_apto(aptidao):
+def mais_apto(aptidao_apto):
     apto = 0
-    for i in range(0, len(aptidao)):
-        if apto < aptidao[i]:
-            apto = aptidao[i]
+    for i in range(0, len(aptidao_apto)):
+        if apto < aptidao_apto[i]:
+            apto = aptidao_apto[i]
     return apto
 
 
 c = csv.writer(open("aptos.csv", "w"))
 d = csv.writer(open("media.csv", "w"))
 for var1 in range(0, 10):
-    populacao = criar_populacao(tamanho_populacao, tamanho_codigo)
+    populacao = criar_populacao()
     for var in range(0, 3):
         print(var)
-        apto = []
+        aptos = []
         media_apt = []
         while geracao_atual < geracoes:
             print(geracao_atual)
-            populacao = cruzamento(populacao, tamanho_populacao, taxa_cruzamento)
+            populacao = cruzamento(populacao)
             populacao = mutacao(populacao, taxa_mutacao)
             aptidao = avaliacao_aptidao(populacao)
-            apto.append(mais_apto(aptidao))
+            aptos.append(mais_apto(aptidao))
+            if mais_apto(aptidao) > melhor_individuo:
+                melhor_individuo = mais_apto(aptidao)
             media_apt.append(sum(aptidao)/len(aptidao))
             geracao_atual += 1
 
-        c.writerow(apto)
+        c.writerow(aptos)
         d.writerow(media_apt)
         geracao_atual = 0
 
 
 media_apt = []
-apto = []
-for i in range(0, geracoes):
+aptos = []
+for p in range(0, geracoes):
     media_apt.append(0)
-    apto.append(0)
+    aptos.append(0)
 with open('media.csv', 'r') as media:
     reader = csv.reader(media, delimiter=',')
 
@@ -158,22 +161,29 @@ with open('media.csv', 'r') as media:
             media_apt[var] += float(valor)
             var += 1
 
-with open('aptos.csv', 'r') as aptos:
-    reader = csv.reader(aptos, delimiter=',')
+with open('aptos.csv', 'r') as ap:
+    reader = csv.reader(ap, delimiter=',')
 
     for linha in reader:
         var = 0
         for valor in linha:
-            apto[var] += float(valor)
+            aptos[var] += float(valor)
             var += 1
 
     for indice in range(0, len(media_apt)):
         media_apt[indice] /= 30
-        apto[indice] /= 30
+        aptos[indice] /= 30
 
+print("Melhor indivíduo:")
+print(melhor_individuo)
 
-f, vetor = plt.subplots(2)
-vetor[0].plot(eixo, apto)
-vetor[1].plot(eixo, media_apt)
+plt.subplot(2, 1, 1)
+plt.plot(eixo, aptos)
+plt.ylabel('Melhor indivíduo')
+
+plt.subplot(2, 1, 2)
+plt.plot(eixo, media_apt)
+plt.xlabel('Gerações')
+plt.ylabel('Média de todos os indivíduos')
 
 plt.show()
