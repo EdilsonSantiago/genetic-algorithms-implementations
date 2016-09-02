e9140929c686a720bb0815e8e5162618e3f2ec0b
Edilson Santiago <edilson.engcomp@gmail.com>
2016-09-02 11:58:00 -0300

    media e des padrao ag

--- AG_representacao_real/agreal.py
+++ AG_representacao_real/agreal.py
@@ -3,7 +3,7 @@ __author__ = 'edilson'
 import numpy as np
 # from random import randint, uniform
 # import matplotlib.pyplot as plt
-# import csv
+import csv
 
 tamanho_populacao = 50
 tamanho_populacao_cruzamento = tamanho_populacao
@@ -52,9 +52,34 @@ def menos_apto():
     pass
 
 
-def media():
-    pass
+def media(arquivo):
+    vetor_media = []
+    for p in range(0, geracoes):
+        vetor_media.append(0)
+    with open(arquivo, 'r') as arq:
+        reader = csv.reader(arq, delimiter=',')
+
+        for linha in reader:
+            var_indice = 0
+            for valor in linha:
+                vetor_media[var_indice] += float(valor)
+                var_indice += 1
+    for indice in range(0, geracoes):
+        vetor_media[indice] /= 30
+    return vetor_media
 
 
-def desvio_padrao():
-    pass
+def desv_padrao(arquivo, vetor_padrao):
+    soma_desvio = np.zeros(geracoes - 1)
+    print(soma_desvio)
+    with open(arquivo, 'r') as ar:
+        reader = csv.reader(ar, delimiter=',')
+        for linha in reader:
+            va = 0
+            for valor in linha:
+                soma_desvio[va] += ((float(valor) - vetor_padrao[va]) ** 2)
+                va += 1
+    for indice in range(0, len(soma_desvio)):
+        soma_desvio[indice] /= 30
+    desvio_padrao = np.sqrt(soma_desvio)
+    return desvio_padrao
