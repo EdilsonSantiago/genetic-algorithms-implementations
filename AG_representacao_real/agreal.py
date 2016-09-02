69d4b44ed38fda52893da14a44dfb11ed3ab9544
Edilson Santiago <edilson.engcomp@gmail.com>
2016-09-02 11:03:26 -0300

    AG com representacao real

---
+++ AG_representacao_real/agreal.py
@@ -0,0 +1,60 @@
+__author__ = 'edilson'
+
+import numpy as np
+# from random import randint, uniform
+# import matplotlib.pyplot as plt
+# import csv
+
+tamanho_populacao = 50
+tamanho_populacao_cruzamento = tamanho_populacao
+geracoes = 200
+melhor_individuo = 0
+melhor_individuo_cromossomo = []
+geracao_atual = 0
+taxa_cruzamento = 75
+taxa_mutacao = 1
+eixo = np.linspace(0, 199, 200)
+
+
+def criar_populacao():
+    pass
+
+
+def normalizacao_linear():
+    pass
+
+
+def avaliacao_aptidao():
+    pass
+
+
+def selecao():
+    pass
+
+
+def elitismo():
+    pass
+
+
+def cruzamento():
+    pass
+
+
+def mutacao():
+    pass
+
+
+def mais_apto():
+    pass
+
+
+def menos_apto():
+    pass
+
+
+def media():
+    pass
+
+
+def desvio_padrao():
+    pass
